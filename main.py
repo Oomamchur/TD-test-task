ccd1749b296d77db2d52e7ddec83ad67cdfa5abb
Oleksandr Mamchur <irex1988@gmail.com>
2024-03-08 18:42:49 +0200

    fixed Exeptions

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import telegram
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 
-import schemas
+from schemas import TelegramMessageBase
 
 app = FastAPI()
 
 
 @app.post("/")
-async def send_message(message: schemas.TelegramMessageBase) -> dict:
+async def send_message(message: TelegramMessageBase) -> dict:
     try:
         bot = telegram.Bot(token=message.bottoken)
         await bot.send_message(chat_id=message.chatid, text=message.message)
@@ -15,4 +15,4 @@ async def send_message(message: schemas.TelegramMessageBase) -> dict:
         return {"Message_send": message.message}
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Check your post request: {str(e)}")
+        return {"Check your post request": f"{type(e).__name__}: {str(e)}"}
